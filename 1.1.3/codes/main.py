a0d131a88b96d4e3311b9eebb5f981d43b7060fb
Harsha122412 <139646786+Harsha122412@users.noreply.github.com>
2023-08-13 15:39:27 +0530

    Update main.py

--- 1.1.3/codes/main.py
+++ 1.1.3/codes/main.py
@@ -19,6 +19,6 @@ Mat = np.array([[1,1,1],[A[0],B[0],C[0]],[A[1],B[1],C[1]]])
 rank = np.linalg.matrix_rank(Mat)
 
 if (rank==2):
-	print("Hence proved that points A,G,D in a triangle are collinear")
+	print("Hence proved that points A,B,C in a triangle are collinear")
 else:
-	print("The given points are not collinear")
+	print("The points A,B,C are not collinear")
