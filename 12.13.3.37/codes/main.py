c77ad790497a168627db9219ca066e05697af020
Harsha122412 <139646786+Harsha122412@users.noreply.github.com>
2023-09-11 03:23:16 +0530

    Update main.py

--- 12.13.3.37/codes/main.py
+++ 12.13.3.37/codes/main.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 def mean(a,b):
-	return np.sum(a@b)
+	return a@b
 def Ex2(a,b):
 	a2=np.square(a)
-	return np.sum(a2@b)
+	return a2@b
 x=np.array([0,1,2,3,4,5])
 px=np.array([float(1/6),float(5/18),float(2/9),float(1/6),float(1/9),float(1/18)])
 
